e8a1c6dfbf0c88a90655a0ad74c75146862161ba
Kanushka Gayan <kanushkanet@gmail.com>
2024-04-18 19:00:36 +0530

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const os = require("os");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send({ status: "OK", message: "Posts app is up and running" });
+});
+
+app.get("/posts", async (req, res) => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    res.send(posts);
+});
+
+// Run the server
+const PORT = process.env.PORT || 8000;
+
+function getLocalIpAddress() {
+    const interfaces = os.networkInterfaces();
+    for (const devName in interfaces) {
+        const iface = interfaces[devName];
+
+        for (let i = 0; i < iface.length; i++) {
+            const alias = iface[i];
+            if (alias.family === "IPv4" && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+    return "localhost";
+}
+
+app.listen(PORT, () => {
+    const ip = getLocalIpAddress();
+    console.log(`Server running at http://${ip}:${PORT}`);
+});
